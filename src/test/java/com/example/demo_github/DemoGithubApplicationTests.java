9909200eb35e725f7614b2826bd5ae5bc46927b8
anna2047 <makalouannamaka@gmail.com>
2026-03-24 01:31:45 +0000

    Reparation du test

--- src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
+++ src/test/java/com/example/demo_github/DemoGithubApplicationTests.java
@@ -2,14 +2,12 @@ package com.example.demo_github;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DemoGithubApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(false, "Erreur volontaire"); // Cette ligne fera planter le test
 	}
 
 }
